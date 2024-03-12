fba8fa611d49ecbce68035b61e65277e2e1d5374
Ivan Kalinin <k.beaver.i03@mail.ru>
2024-03-12 03:21:47 +0300

    For Vanya

--- Kom_graphics_Laba_2/Form1.Designer.cs
+++ Kom_graphics_Laba_2/Form1.Designer.cs
@@ -28,13 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Drow = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1072, 501);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;            // 
+            // Drow
+            // 
+            this.Drow.BackColor = System.Drawing.SystemColors.Control;
+            this.Drow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Drow.Location = new System.Drawing.Point(1108, 497);
+            this.Drow.Name = "Drow";
+            this.Drow.Size = new System.Drawing.Size(165, 69);
+            this.Drow.TabIndex = 1;
+            this.Drow.Text = "Отрисовать";
+            this.Drow.UseVisualStyleBackColor = false;
+            this.Drow.Click += new System.EventHandler(this.Drow_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1108, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1108, 132);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1108, 88);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1108, 176);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1105, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Смещение по X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1132, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1104, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Смещение по Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1132, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1325, 588);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Drow);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Drow;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Kom_graphics_Laba_2/Form1.cs
+++ Kom_graphics_Laba_2/Form1.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,61 @@ namespace Kom_graphics_Laba_2
 {
     public partial class Form1 : Form
     {
+        Graphics g;
         public Form1()
         {
             InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Draw();
+        }
+        public void Draw()
+        {
+            int W = this.pictureBox1.Width;
+            int H = this.pictureBox1.Height;
+            int x_center = W / 2;
+            int y_center = H / 2;
+            int dx = W / 30;
+            int dy = H / 30;
+            g.DrawLine(Pens.Black, x_center, 0, x_center, H);
+            g.DrawLine(Pens.Black, 0, y_center, W, y_center);
+            Font Fon = new Font("Arial", 9, FontStyle.Regular);
+            Brush Br = Brushes.Black;
+            g.DrawString("X", Fon, Br, W - 15, y_center + 10);
+            g.DrawString("Y", Fon, Br, x_center - 20, 10);
+            Font Fon1 = new Font("Arial", 10, FontStyle.Regular);
+            for (int i = -15; i < 15; i++)
+            {
+                g.DrawString(i.ToString(), Fon1, Br, x_center - 15, y_center + dy * i);
+                g.DrawString(i.ToString(), Fon1, Br, x_center + dx * i - 10, y_center + 10);
+            }
+            Point[] CurrectPoint = new Point[17];
+            CurrectPoint[0] = (new Point(x_center, y_center));
+            CurrectPoint[1] = (new Point(x_center + dx * 3, y_center + dy * -3));
+            CurrectPoint[2] = (new Point(x_center + dx * 3, y_center + dy * -5));
+            CurrectPoint[3] = (new Point(x_center + dx * 2, y_center + dy * -5));
+            CurrectPoint[4] = (new Point(x_center + dx * 3, y_center + dy * -7));
+            CurrectPoint[5] = (new Point(x_center + dx * 1, y_center + dy * -7));
+            CurrectPoint[6] = (new Point(x_center + dx * 0, y_center + dy * -9));
+            CurrectPoint[7] = (new Point(x_center + dx * -1, y_center + dy * -7));
+            CurrectPoint[8] = (new Point(x_center + dx * -3, y_center + dy * -7));
+            CurrectPoint[9] = (new Point(x_center + dx * -2, y_center + dy * -5));
+            CurrectPoint[10] = (new Point(x_center + dx * -3, y_center + dy * -5));
+            CurrectPoint[11] = (new Point(x_center + dx * -3, y_center + dy * -3));
+            CurrectPoint[12] = (new Point(x_center, y_center));
+            CurrectPoint[13] = (new Point(x_center + dx * 3, y_center + dy * -3));
+            CurrectPoint[14] = (new Point(x_center + dx * -3, y_center + dy * -3));
+            CurrectPoint[15] = (new Point(x_center + dx * -3, y_center + dy * -5));
+            CurrectPoint[16] = (new Point(x_center + dx * 3, y_center + dy * -5));
+            Pen pen = new Pen(Color.Black);
+            g.DrawLines(pen, CurrectPoint);
+        }
+
+        private void Drow_Click(object sender, EventArgs e)
+        {
+            Draw();
         }
     }
 }
